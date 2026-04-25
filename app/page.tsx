f784803ce8294a54dddc3cd6f3e9ab252e8abed5
NabilApipp <nabilapipp@gmail.com>
2026-04-26 06:53:13 +0700

    chore: update main path to /registrasi

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // redirect("/registrasi");
+  redirect("/registrasi");
   // redirect("/maintenance");
-  redirect("/coming-soon");
+  // redirect("/coming-soon");
 }
